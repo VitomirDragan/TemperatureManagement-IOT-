f560bb97b23fc923e02a278d4275ee331554a19d
VitomirDragan <vitomir_dragan86@yahoo.com>
2020-11-29 19:26:50 +0200

    Fixed timing bug

--- SoftwareForESP8266/SoftwareForESP8266.ino
+++ SoftwareForESP8266/SoftwareForESP8266.ino
@@ -1,4 +1,5 @@
 #include "WiFiModule.h"
+#include <String.h>
 
 WiFiModule wifiModule;
 
@@ -22,42 +23,72 @@ int getWeekDay() {
     return getPointerToTMStruct()->tm_wday; //returning weekday(int value from 0 to 6, 0 - Sunday, 6 - Saturday)
 }
 
-String getCurrentTime() {
+int getCurrentHour() {
     tm *timeStruct = getPointerToTMStruct();
-    return String(timeStruct->tm_hour) + ":" + String(timeStruct->tm_min); //returning current time(format hh:mm)
+    return timeStruct->tm_hour;
+}
+
+int getCurrentMinute(){
+    tm *timeStruct = getPointerToTMStruct();
+    return timeStruct->tm_min;
+}
+
+int getHourFromTimeFormat(String timeFormat)
+{
+  return timeFormat.substring(0,timeFormat.indexOf(":")).toInt();
+}
+
+int getMinuteFromTimeFormat(String timeFormat)
+{
+  return timeFormat.substring(timeFormat.indexOf(":")+1).toInt();
 }
 
 
-void loop() {
+void loop() {  
     int dataPackage = wifiModule.readDataFromArduino();
     if (dataPackage % 10 == 1)
         wifiModule.sendDesiredTemperatureToDatabase(wifiModule.readDesiredTemperatureFromArduino(dataPackage));
     int switchTimingOn = wifiModule.readInt("SwitchTimingOn/Value");
     if (switchTimingOn) {
         int weekDay = getWeekDay();
-        String timeNow = getCurrentTime();
+        int currentHour = getCurrentHour();
+        int currentMinute = getCurrentMinute();
 
         if (weekDay == 6 || weekDay == 0) {
-            String a = wifiModule.readStr("Weekend/A");
-            String b = wifiModule.readStr("Weekend/B");
+            String A = wifiModule.readStr("Weekend/A");
+            String B = wifiModule.readStr("Weekend/B");
+            
+            int hourA = getHourFromTimeFormat(A);
+            int minuteA = getMinuteFromTimeFormat(A);
+            int hourB = getHourFromTimeFormat(B);
+            int minuteB = getMinuteFromTimeFormat(B);
 
-            if (a <= timeNow && timeNow < b) {
+            if ((hourA < currentHour && currentHour < hourB) || (hourA==currentHour && currentMinute >= minuteA) || (hourB==currentHour && currentMinute < minuteB)) {
                 wifiModule.sendDesiredTemperatureToArduino(wifiModule.readInt("Weekend/TemperatureAB"));
             } else {
                 wifiModule.sendDesiredTemperatureToArduino(wifiModule.readInt("Weekend/TemperatureBA"));
             }
 
         } else {
-            String a = wifiModule.readStr("WorkingDay/A");
-            String b = wifiModule.readStr("WorkingDay/B");
-            String c = wifiModule.readStr("WorkingDay/C");
-            String d = wifiModule.readStr("WorkingDay/D");
+            String A = wifiModule.readStr("WorkingDay/A");
+            String B = wifiModule.readStr("WorkingDay/B");
+            String C = wifiModule.readStr("WorkingDay/C");
+            String D = wifiModule.readStr("WorkingDay/D");
 
-            if (a <= timeNow && timeNow <= b) {
+            int hourA = getHourFromTimeFormat(A);
+            int minuteA = getMinuteFromTimeFormat(A);
+            int hourB = getHourFromTimeFormat(B);
+            int minuteB = getMinuteFromTimeFormat(B);
+            int hourC = getHourFromTimeFormat(C);
+            int minuteC = getMinuteFromTimeFormat(C);
+            int hourD = getHourFromTimeFormat(D);
+            int minuteD = getMinuteFromTimeFormat(D);            
+
+            if ((hourA < currentHour && currentHour < hourB) || (hourA==currentHour && currentMinute >= minuteA) || (hourB==currentHour && currentMinute < minuteB)) {
                 wifiModule.sendDesiredTemperatureToArduino(wifiModule.readInt("WorkingDay/TemperatureAB"));
-            } else if (b < timeNow && timeNow <= c) {
+            } else if ((hourB < currentHour && currentHour < hourC) || (hourB==currentHour && currentMinute >= minuteB) || (hourC==currentHour && currentMinute < minuteC)) {
                 wifiModule.sendDesiredTemperatureToArduino(wifiModule.readInt("WorkingDay/TemperatureBC"));
-            } else if (c < timeNow && timeNow <= d) {
+            } else if ((hourC < currentHour && currentHour < hourD) || (hourC==currentHour && currentMinute >= minuteC) || (hourD==currentHour && currentMinute < minuteD)) {
                 wifiModule.sendDesiredTemperatureToArduino(wifiModule.readInt("WorkingDay/TemperatureCD"));
             } else {
                 wifiModule.sendDesiredTemperatureToArduino(wifiModule.readInt("WorkingDay/TemperatureDA"));
